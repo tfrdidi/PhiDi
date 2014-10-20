c0ae5e7550b5ecb286404d74c75a465520c61927
Lukas Hahmann <tfr.didi@gmail.com>
2014-10-20 18:30:57 +0200

    changed the name of phiDiLib to backend, removed phidi.py, so that now methods are called that only return the distance

---
+++ backend/phidi.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import sys, phidilib
+
+
+baseURI = "https://de.wikipedia.org"
+wikiSuffix = "/wiki/"
+target = "Philosophie"
+maxDistance = 20
+
+
+
+def getSampleDistance():
+	return getDistanceTo("mallorca")
+	
+
+def getDistanceTo(articleName):
+	subPath = wikiSuffix + articleName
+	distance = -1
+	targetNotReached = True
+
+	while(targetNotReached and (distance < maxDistance)):
+		if(subPath == wikiSuffix + target):
+			targetNotReached = False
+		else:
+			subPath = phidilib.getForwardingLink(baseURI + str(subPath))
+		distance += 1
+	return distance
+
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
