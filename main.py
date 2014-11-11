864036723a935c40f20beb3c0a1ec86d3a75ed2f
tfrdidi <tfr.didi@gmail.com>
2014-11-11 22:40:32 +0100

    removed a naming bug from main, loaded philosophers page now

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-#from flask import Flask
 from phiDi import phidi
 from phiDi import loadPage
+
 import webapp2
-
-#app = Flask(__name__)
-#app.config['DEBUG'] = True
-
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
 
 
 class MainPage(webapp2.RequestHandler):
 
   def get(self):  # pylint:disable-msg=invalid-name
-    """Handle GET requests."""
-    self.response.header['Content-Type'] = "text/plain"
-    self.response.out.write("Hello world")
+    return open(os.path.dirname(__file__) + "/pages/philosophers.html")
 
 
 class PhidiHandler(webapp2.RequestHandler):
@@ -25,5 +17,5 @@ class PhidiHandler(webapp2.RequestHandler):
         self.response.out.write("your input was: " + q)
 
 
-app = webapp2.WSGIApplication([('/.*', MainHandler), ('/phidi', PhidiHandler)], debug=True)
+app = webapp2.WSGIApplication([('/.*', MainPage), ('/phidi', PhidiHandler)], debug=True)
 
